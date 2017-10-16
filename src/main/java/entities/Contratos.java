90bb720476ed89cbbf29ccebf26296669cc981e7
kevin <kevin@asus-x555LAB>
2017-10-15 21:38:12 -0600

    Pantilla para los contratos creada

--- src/main/java/entities/Contratos.java
+++ src/main/java/entities/Contratos.java
@@ -9,6 +9,13 @@ public class Contratos implements Serializable{
    private String nombres;
    private String apellidos;
    private String fecha;
+   private String primero;
+   private String segundo;
+   private String tercero;
+   private String cuarto;
+   private String cinco;
+   private String datosPersonales;
+   private String conveniente;
 
     public String getNombres() {
         return nombres;
@@ -33,6 +40,63 @@ public class Contratos implements Serializable{
     public void setFecha(String fecha) {
         this.fecha = fecha;
     }
-   
+
+    public String getPrimero() {
+        return primero;
+    }
+
+    public void setPrimero(String primero) {
+        this.primero = primero;
+    }
+
+    public String getSegundo() {
+        return segundo;
+    }
+
+    public void setSegundo(String segundo) {
+        this.segundo = segundo;
+    }
+
+    public String getTercero() {
+        return tercero;
+    }
+
+    public void setTercero(String tercero) {
+        this.tercero = tercero;
+    }
+
+    public String getCuarto() {
+        return cuarto;
+    }
+
+    public void setCuarto(String cuarto) {
+        this.cuarto = cuarto;
+    }
+
+    public String getCinco() {
+        return cinco;
+    }
+
+    public void setCinco(String cinco) {
+        this.cinco = cinco;
+    }
+
+    public String getDatosPersonales() {
+        return datosPersonales;
+    }
+
+    public void setDatosPersonales(String datosPersonales) {
+        this.datosPersonales = datosPersonales;
+    }
+
+    public String getConveniente() {
+        return conveniente;
+    }
+
+    public void setConveniente(String conveniente) {
+        this.conveniente = conveniente;
+    }
+    
+    
    
 }
--- src/main/java/managedbeansv3/FrmContratos.java
+++ src/main/java/managedbeansv3/FrmContratos.java
@@ -11,9 +11,9 @@ import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 import javax.faces.context.FacesContext;
-import javax.faces.view.ViewScoped;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import net.sf.jasperreports.engine.JRException;
@@ -28,44 +28,101 @@ import net.sf.jasperreports.engine.export.ooxml.JRDocxExporter;
 import net.sf.jasperreports.engine.export.ooxml.JRPptxExporter;
 
 @Named(value = "frmContratos")
-@ViewScoped
+@RequestScoped
 public class FrmContratos implements Serializable{
 
     public FrmContratos() {
     }
     
-    private List<Contratos> personas = new ArrayList<Contratos>();
+    private String parametro1= "forma de pasar paramtros";
 
-	public List<Contratos> getPersonas() {
-		Contratos per = new Contratos();
-		per.setNombres("Kevin Vicente");
-		per.setApellidos("Martinez");
-		Calendar cal = Calendar.getInstance();
-		cal.set(1991, 1, 21);
-		per.setFecha(String.valueOf(cal.getTime()));
-		personas.add(per);
-		
-		per = new Contratos();
-		per.setNombres("Walter");
-		per.setApellidos("Ramirez");
-		cal = Calendar.getInstance();
-		cal.set(1990, 5, 21);
-		per.setFecha(String.valueOf(cal.getTime()));
-		personas.add(per);
-		
-		return personas;
-	}
+    public String getParametro1() {
+        return parametro1;
+    }
 
-    public void setPersonas(List<Contratos> personas) {
-        this.personas = personas;
+    public void setParametro1(String parametro1) {
+        this.parametro1 = parametro1;
     }
     
-        public void verPDF(ActionEvent actionEvent) throws Exception{
+    private List<Contratos> datos = new ArrayList<Contratos>();
+
+    public List<Contratos> getDatos() {
+        
+        Calendar cal = Calendar.getInstance();
+	cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DATE));
+        String fechaActual=String.valueOf(cal.getTime());
+        
+        String primero=null;
+        String segundo=null;
+        String tercero=null;
+        String cuarto= null;
+        String cinco= null;
+        String datosPersonales= null;
+        String conveniente=null;
+        
+        String nombres="Walter Jacobo";
+        String apellidos="Ramirez Lopez";
+        String dui="98998882-8";
+        String direccion= "Tes palos de coco avenida sur";
+        String ciudad="Santa Ana";
+        double monto=25660.33;
+        String fecha="12-05-2017";
+        double interes= 0.5;
+        String fechaMaxima="15-05-2020";
+        
+        
+        datosPersonales="Conste por el presente contrato Privado de Préstamo de Dinero que celebramos "
+                + "de una parte la Señor/a. "+ nombres +", identificada con DUI. Nro. "+dui+","
+                + "domiciliada en "+direccion+" de esta ciudad de "+ciudad+" a quien en adelante "
+                + "sele denominara a quien en adelante se le denominará el PRESTATARIO, y de la otra parte "
+                + "el PRESTAMISTA.";
+        conveniente="Ambas partes llegan a los acuerdos siguientes:";
+        
+        primero= "LA PRESTAMISTA cede en calidad de PRESTAMO al PRESTATARIO la suma $"+monto+" a la "
+                + "Señor/a "+nombres+" "+apellidos+" La de fecha del "+fecha+".";
+        segundo= "El prestatario acepta dicho dinero en calidad deprestamo y asegura haber recibido "
+                + "el total del dinero a la firma del presente documento, por lo que se compromete "
+                + "a devolver dicha suma de dinero, asimismo ambas partes acuerdan quedicho prestamo "
+                + "generará el interes de "+interes+" por ciento.";
+        tercero= "El capital prestado ha de devolverse en un plazo máximo de "+fechaMaxima+" años. No obstante"
+                + "el restatario podrá amortizar de forma anticipada el capital pendiente en "
+                + "cualquier momento. Los intereses se abonrán al efectuar el pago del capital en"
+                + "cada uno de los plazos.";
+        cuarto="En caso de incumplimiento de parte del PRESTATARIO A LA PRESTAMISTA queda facultada a"
+                + " recurrir a las autoridades pertinentes y hacer valer sus derechos,"
+                + " por loque el presente documento es suficiente medio probatorio y vale como RECIBO.";
+        cinco= "Ambas partes señalan y aseguran que en lacelebración del mismo no ha mediado error, "
+                + "dolo de nulidado anulabilidad que pudiera invalidar el contenido del mismo, por lo "
+                + "que proceden a firmar en la ciudad de Santa Ana, fecha "+fechaActual+"";
+        
+        
+        Contratos info = new Contratos();
+        
+        info.setDatosPersonales(datosPersonales);
+        info.setPrimero(primero);
+        info.setSegundo(segundo);
+        info.setTercero(tercero);
+        info.setCuarto(cuarto);
+        info.setCinco(cinco);
+        info.setConveniente(conveniente);
+	info.setFecha(fechaActual);
+	datos.add(info);
+		
+	return datos;
+    }
+
+    public void setDatos(List<Contratos> datos) {
+        this.datos = datos;
+    }
+
+    
+    
+        public void verPDF() throws Exception{
                 Map<String,Object> parametros= new HashMap<String,Object>();
-		parametros.put("txtUsuario", "Cacheton");
+		parametros.put("txtUsuario", parametro1);
             
 		File jasper = new File(FacesContext.getCurrentInstance().getExternalContext().getRealPath("/resources/reports/rptContratos.jasper"));
-		byte[] bytes = JasperRunManager.runReportToPdf(jasper.getPath(), parametros, new JRBeanCollectionDataSource(this.getPersonas()));
+		byte[] bytes = JasperRunManager.runReportToPdf(jasper.getPath(), parametros, new JRBeanCollectionDataSource(this.getDatos()));
 		
                 HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
 		response.setContentType("application/pdf");
@@ -79,15 +136,15 @@ public class FrmContratos implements Serializable{
 		FacesContext.getCurrentInstance().responseComplete();
 	}
     
-    	public void exportarPDF(ActionEvent actionEvent) throws JRException, IOException{
+    	public void exportarPDF() throws JRException, IOException{
 		Map<String,Object> parametros= new HashMap<String,Object>();
-		parametros.put("txtUsuario", "Cacheton");
+		parametros.put("txtUsuario", parametro1);
 		
 		File jasper = new File(FacesContext.getCurrentInstance().getExternalContext().getRealPath("/resources/reports/rptContratos.jasper"));
-		JasperPrint jasperPrint = JasperFillManager.fillReport(jasper.getPath(),parametros, new JRBeanCollectionDataSource(this.getPersonas()));
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasper.getPath(),parametros, new JRBeanCollectionDataSource(this.getDatos()));
 		
 		HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
-		response.addHeader("Content-disposition","attachment; filename=jsfReporte.pdf");
+		response.addHeader("Content-disposition","attachment; filename=contrato-sis_prestamos.pdf");
 		ServletOutputStream stream = response.getOutputStream();
 		
 		JasperExportManager.exportReportToPdfStream(jasperPrint, stream);
@@ -97,15 +154,15 @@ public class FrmContratos implements Serializable{
 		FacesContext.getCurrentInstance().responseComplete();
 	}
 	
-	public void exportarExcel(ActionEvent actionEvent) throws JRException, IOException{
+	public void exportarExcel() throws JRException, IOException{
 		Map<String,Object> parametros= new HashMap<String,Object>();
-		parametros.put("txtUsuario", "Cacheton");
+		parametros.put("txtUsuario", parametro1);
 		
 		File jasper = new File(FacesContext.getCurrentInstance().getExternalContext().getRealPath("/resources/reports/rptContratos.jasper"));
-		JasperPrint jasperPrint = JasperFillManager.fillReport(jasper.getPath(),parametros, new JRBeanCollectionDataSource(this.getPersonas()));
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasper.getPath(),parametros, new JRBeanCollectionDataSource(this.getDatos()));
 		
 		HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
-		response.addHeader("Content-disposition","attachment; filename=jsfReporte.xlsx");
+		response.addHeader("Content-disposition","attachment; filename=contrato-sis_prestamos.xlsx");
 		ServletOutputStream stream = response.getOutputStream();
 		
 		JRXlsExporter exporter = new JRXlsExporter();
@@ -118,15 +175,15 @@ public class FrmContratos implements Serializable{
 		FacesContext.getCurrentInstance().responseComplete();
 	}
 	
-	public void exportarPPT(ActionEvent actionEvent) throws JRException, IOException{
+	public void exportarPPT() throws JRException, IOException{
 		Map<String,Object> parametros= new HashMap<String,Object>();
-		parametros.put("txtUsuario", "Cacheton");
+		parametros.put("txtUsuario", parametro1);
 		
 		File jasper = new File(FacesContext.getCurrentInstance().getExternalContext().getRealPath("/resources/reports/rptContratos.jasper"));
-		JasperPrint jasperPrint = JasperFillManager.fillReport(jasper.getPath(),parametros, new JRBeanCollectionDataSource(this.getPersonas()));
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasper.getPath(),parametros, new JRBeanCollectionDataSource(this.getDatos()));
 		
 		HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
-		response.addHeader("Content-disposition","attachment; filename=jsfReporte.ppt");
+		response.addHeader("Content-disposition","attachment; filename=contrato-sis_prestamos.ppt");
 		ServletOutputStream stream = response.getOutputStream();
 		
 		JRPptxExporter exporter = new JRPptxExporter();
@@ -139,15 +196,15 @@ public class FrmContratos implements Serializable{
 		FacesContext.getCurrentInstance().responseComplete();
 	}
 	
-	public void exportarDOC(ActionEvent actionEvent) throws JRException, IOException{
+	public void exportarDOC() throws JRException, IOException{
 		Map<String,Object> parametros= new HashMap<String,Object>();
-		parametros.put("txtUsuario", "Cacheton");
+		parametros.put("txtUsuario", parametro1);
 		
 		File jasper = new File(FacesContext.getCurrentInstance().getExternalContext().getRealPath("/resources/reports/rptContratos.jasper"));
-		JasperPrint jasperPrint = JasperFillManager.fillReport(jasper.getPath(),parametros, new JRBeanCollectionDataSource(this.getPersonas()));
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasper.getPath(),parametros, new JRBeanCollectionDataSource(this.getDatos()));
 		
 		HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
-		response.addHeader("Content-disposition","attachment; filename=jsfReporte.doc");
+		response.addHeader("Content-disposition","attachment; filename=contrato-sis_prestamos.doc");
 		ServletOutputStream stream = response.getOutputStream();
 		
 		JRDocxExporter exporter = new JRDocxExporter();
