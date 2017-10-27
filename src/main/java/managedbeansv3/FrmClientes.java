3b0621f017093cacfa894042ce3616d2f8c15204
Your Name <you@example.com>
2017-10-26 23:00:49 -0600

    merged

--- src/main/java/managedbeansv3/FrmClientes.java
+++ src/main/java/managedbeansv3/FrmClientes.java
@@ -5,7 +5,9 @@
  */
 package managedbeansv3;
 
+import entities.Bitacora;
 import entities.Cliente;
+import entities.Usuario;
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
@@ -17,13 +19,16 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
 import javax.servlet.ServletContext;
+import manager.ControladorBitacora;
 import manager.ControladorCliente;
 import manager.ControladorDocumentos;
 import org.apache.commons.io.FileUtils;
@@ -38,7 +43,15 @@ import services.Conexion;
  */
 @Named(value = "frmClientes")
 @ViewScoped
-public class FrmClientes implements Serializable{
+public class FrmClientes implements Serializable {
+
+    private Usuario usuario = new Usuario();
+    private Bitacora bit = new Bitacora();
+    private ControladorBitacora cbit = new ControladorBitacora();
+
+    private ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+    private Map<String, Object> sessionMap = externalContext.getSessionMap();
+    Usuario sesion = (Usuario) sessionMap.get("user");
 
     ControladorCliente ccliente = new ControladorCliente();
     DateFormat df = new SimpleDateFormat("yyyy-mm-dd");
@@ -51,35 +64,35 @@ public class FrmClientes implements Serializable{
     MensajesFormularios mensaje = new MensajesFormularios(); //Mensajes de validacion
     MetodosShare metodo = new MetodosShare();
     private ControladorDocumentos doc = new ControladorDocumentos();
-    
-    Conexion cn= new Conexion();
-    
+
+    Conexion cn = new Conexion();
+
     public void imagen(FileUploadEvent event) {
         UploadedFile file = event.getFile();
         String nombre = file.getFileName();
-        System.out.println(nombre);    
+        System.out.println(nombre);
 
     }
-    
-    public void handleFileUpload(FileUploadEvent event) throws IOException{
+
+    public void handleFileUpload(FileUploadEvent event) throws IOException {
         int correlativo = doc.obtenerMaxId(nuevoCliente.getDui());
-        if(event.getFile()!=null){
-           
-            try{
+        if (event.getFile() != null) {
+
+            try {
                 cn.UID("INSERT INTO documento(dui, correlativo, nombre_archivo, archivo, descripcion)"
-                + " VALUES('" + nuevoCliente.getDui() + "','" + correlativo + "','" +
-                event.getFile().getFileName() + "','" +
-                event.getFile().getInputstream() + "','" + "IMAGEN"+"')");
+                        + " VALUES('" + nuevoCliente.getDui() + "','" + correlativo + "','"
+                        + event.getFile().getFileName() + "','"
+                        + event.getFile().getInputstream() + "','" + "IMAGEN" + "')");
                 nuevo();
-            }catch(Exception e){
+            } catch (Exception e) {
                 Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, e);
             }
-        }else{
-           System.out.println("El archivo o imagen se encuentra vacio");
-       }
-     
+        } else {
+            System.out.println("El archivo o imagen se encuentra vacio");
+        }
+
     }
-            
+
     public FrmClientes() {
     }
 
@@ -91,6 +104,7 @@ public class FrmClientes implements Serializable{
         try {
             if (nuevoCliente.Validar()) {
                 nuevoCliente.fechaNacimiento = metodo.utilDatetoSqlDate(nuevoCliente.getFechaNacimiento().toString());
+                generarAccion("Se creo el cliente con dui :" + nuevoCliente.getDui());
                 ccliente.agregar(nuevoCliente);
             } else {
                 mensaje.msgFaltanCampos();
@@ -103,6 +117,7 @@ public class FrmClientes implements Serializable{
     public void eliminarCliente() throws Exception {
         if (scliente.getDui().isEmpty() != true) {
             ccliente.eliminar(scliente);
+            generarAccion("Se elimino el cliente con el dui : " + scliente.getDui());
             scliente = new Cliente();
         }
     }
@@ -119,12 +134,17 @@ public class FrmClientes implements Serializable{
     public Cliente buscar(String esta) {
         return ccliente.buscar(esta).get(0);
     }
+    
+     public void generarAccion(String accion) {
 
-   
-    
+        bit.setAccion(accion);
+        bit.setId_usuario(sesion.getId());
+        cbit.agregar(bit);
+        
+        
+    }
+
     /*-------------------------Getter and Setter----------------------------*/
-    
-    
     public List<Cliente> getLcliente() {
         if (busquedaActiva) {
             lcliente = ccliente.buscar(busqueda);
@@ -213,7 +233,5 @@ public class FrmClientes implements Serializable{
     public void setCn(Conexion cn) {
         this.cn = cn;
     }
-    
-    
-    
+
 }
